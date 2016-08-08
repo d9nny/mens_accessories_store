6257952b17354f5fc226a59cce599f43cf778cbf
danwheeler <da-wheeler@hotmail.co.uk>
2016-08-08 20:57:12 +0100

    added two categories for products and a item page

--- app/app.js
+++ app/app.js
@@ -7,11 +7,16 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/", {templateUrl: "app/partials/home.html", controller: "PageCtrl"})
     .when("/about", {templateUrl: "app/partials/about.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "app/partials/contact.html", controller: "PageCtrl"})
-    .when("/products", {templateUrl: "app/partials/products.html", controller: "PageCtrl"})
+    .when("/products", {templateUrl: "app/partials/product.html", controller: "ProductCtrl"})
+    .when("/products/item", {templateUrl: "app/partials/product-item.html", controller: "ProductCtrl"})
     .when("/blog", {templateUrl: "app/partials/blog.html", controller: "PageCtrl"})
     .otherwise("/404", {templateUrl: "app/partials/404.html", controller: "PageCtrl"});
 }]);
 
 app.controller('PageCtrl', function () {
+
+});
+
+app.controller('ProductCtrl', function () {
 
 });
