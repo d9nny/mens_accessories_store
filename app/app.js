a1289f7eecf96e4f61982a1032f22114e5ce930b
danwheeler <da-wheeler@hotmail.co.uk>
2016-08-10 22:42:57 +0100

    Added html for products and product pages - not tested with data yet

--- app/app.js
+++ app/app.js
@@ -6,17 +6,34 @@ app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     .when("/", {templateUrl: "app/partials/home.html", controller: "PageCtrl"})
     .when("/about", {templateUrl: "app/partials/about.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "app/partials/contact.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "app/partials/contact.html", controller: "ContactCtrl"})
     .when("/products", {templateUrl: "app/partials/product.html", controller: "ProductCtrl"})
     .when("/products/item", {templateUrl: "app/partials/product-item.html", controller: "ProductCtrl"})
     .when("/blog", {templateUrl: "app/partials/blog.html", controller: "PageCtrl"})
     .otherwise("/404", {templateUrl: "app/partials/404.html", controller: "PageCtrl"});
 }]);
 
-app.controller('PageCtrl', function () {
+app.controller('ContactCtrl', function () {
 
 });
 
 app.controller('ProductCtrl', function () {
+	var self = this;
 
+	self.hoverIn = function() {
+		self.mouseover = true;
+	}
+
+	self.hoverOut = function() {
+		self.mouseover = false;
+	}
+
+	self.select = function(product) {
+		self.product = product;
+		self.clicked = true;
+	}
+
+	self.closeSelect = function() {
+		self.clicked = false;
+	}
 });
